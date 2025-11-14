b5bc031692171a86ab681219e1175df51a9bfde4
zzzzzyyy222 <chongzhe3@gmail.com>
2025-11-14 15:16:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -511,5 +511,3 @@ elif page == "Prediction":
         st.success(f"Sleep Disorder: **{pred_class}**")
         st.markdown(f"**Recommendation:** {advice.get(pred_class, 'No advice available.')}")
 
-        st.success(f"Sleep Disorder: **{pred_class}**")
-        st.markdown(f"**Recommendation:** {advice.get(pred_class, 'No advice available.')}")
